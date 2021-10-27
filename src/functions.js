3d4c43d4ff37b33eddfde16970792799c3b7fb74
Lino09 <onosmx@gmail.com>
2021-10-27 15:15:32 -0500

    Add module with functionalities

---
+++ src/functions.js
@@ -0,0 +1,32 @@
+export function toggle(el,list) { 
+  list.forEach((task)=> {
+    if(task === el){
+      task.isCompleted = !task.isCompleted
+    }
+  })
+  saveLocal(list)
+}
+
+export function add(list) {
+  list.push({description:document.querySelector('#newTask').value, isCompleted: false, index: list.length});
+  document.querySelector('#newTask').value = '';
+  saveLocal(list)
+}
+
+export function saveLocal(list) {
+window.localStorage.setItem('todos',JSON.stringify(list))
+}
+
+export function updateIndex(list) {
+  let i = 0;
+  list.forEach((el) => {
+    el.index = i;
+    i += 1;
+  })
+}
+
+export function removeDone(list) {
+  list = list.filter((el) => el.isCompleted === false)
+  updateIndex(list)
+  saveLocal(list)
+}
